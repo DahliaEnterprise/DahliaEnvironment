86503712df1c2798a1eef71e04e61f3c81070703
Shane Betz <dahliacommunication67@gmail.com>
2023-10-27 19:03:15 -0700

    *character map iterator pertaining to hideandseek algorithm

--- source/hideandseek/dahliaenvironment_hideandseek.php
+++ source/hideandseek/dahliaenvironment_hideandseek.php
@@ -2,9 +2,39 @@
 
 class dahliaenvironment_hideandseek
 {
+    $character_map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'\";:,<.>/?~`!@#$%^&*()-_=+[{]}|\\";
+
+    function decoration_element_iterator($character, $iterations)
+    {
+        $output = ""+$character;
+
+        //Find index of character contained within the character map
+        $index_of_character_within_character_map = 0;
+        $keep_searching = 1;
+        while($keep_searching == 1)
+        {
+            if( strcmp($character_map[$index_of_character_within_character_map], $character) == 0)
+            {
+                $keep_searching = 0;
+            }else{
+                $index_of_character_within_character_map = $index_of_character_within_character_map + 1;
+                if($index_of_character_within_character_map >= strlen($character_map))
+                {
+                    $keep_searching = 0;
+                }
+            }
+        }
+
+        $output = $character_map[$character_map];
+
+        return $output;
+    }
+
     function hide($decoration_to_hide)
     {
         $output = "";
+
+        //INPUT MUST BE LARGER THAN 32 CHARACTERS.
 
         //For each character iterate using the integer representation of each and every other character(exluding the character to be modified).
         $total_characters = strlen($decoration_to_hide);
@@ -17,7 +47,12 @@ class dahliaenvironment_hideandseek
             while($character_iterator < $total_characters)
             {
 
-                //skip
+                //skip the index pertaining to "character_to_iterate"
+                if($character_iterator != $decoration_to_hide_index)
+                {
+                    //apply iterate
+
+                }
             }
 
             $index = $index + 1;
