563bffb70b10d433c1943f44475355317e4d8ca9
Shane Betz <dahliacommunication67@gmail.com>
2023-10-28 18:26:02 -0700

    *decoration element iterator: output is defined as array, character iterates amongst the character map according to intesity

--- source/hideandseek/dahliaenvironment_hideandseek.php
+++ source/hideandseek/dahliaenvironment_hideandseek.php
@@ -6,31 +6,56 @@ class dahliaenvironment_hideandseek
 
     function decoration_element_iterator($character, $iterations)
     {
-        $output = ""+$character;
+        $output = [];
+        $output["character"] = ""+$character;
+        $output["successful_process"] = -1;
 
         //Find index of character contained within the character map
+        $character_found = 1;
         $index_of_character_within_character_map = 0;
         $keep_searching = 1;
-        while($keep_searching == 1)
+        while( $keep_searching == 1 )
         {
-            if( strcmp($character_map[$index_of_character_within_character_map], $character) == 0)
+            if( strcmp($character_map[$index_of_character_within_character_map], $character) == 0 )
             {
                 $keep_searching = 0;
             }else{
                 $index_of_character_within_character_map = $index_of_character_within_character_map + 1;
-                if($index_of_character_within_character_map >= strlen($character_map))
+                if( $index_of_character_within_character_map >= strlen($character_map) )
                 {
+                    $character_found = 1;
                     $keep_searching = 0;
                 }
             }
         }
 
-        $output = $character_map[$character_map];
+        if( $character_found == 1 )
+        {
+            //=$ index of character found
+            //traverse the index of the character map strength of intended iterations
+            $strength_achieved_intensity_traversed = 0;
+            $character_map_index = $index_of_character_within_character_map;
+            while($strength_achieved_intesity_traversed < $iterations)
+            {
+                $character_map_index = $character_map_index + 1;
+                if($character_map_index >= strlen($character_map))
+                {
+                    //loop to begining of ring buffer
+                    $character_map_index = 0;
+                }
+
+                $strength_achieved_intesity_traversed = $strength_achieved_intesity_traversed + 1;
+            }
+
+            //produce result
+            $output["character"] = "".$character_map[$strength_achieved_intesity_traversed];
+            $output["successful_process"] = 1;
+        }
 
         return $output;
     }
 
-    function hide($decoration_to_hide)
+    function hide( $decoration_to_hide )
     {
         $output = "";
 
@@ -39,12 +64,12 @@ class dahliaenvironment_hideandseek
         //For each character iterate using the integer representation of each and every other character(exluding the character to be modified).
         $total_characters = strlen($decoration_to_hide);
         $decoration_to_hide_index = 0;
-        while($decoration_to_hide_index < $total_characters)
+        while( $decoration_to_hide_index < $total_characters )
         {
             $character_to_iterate = $decoration_to_hide[$decoration_to_hide_index];
 
             $character_iterator = 0;
-            while($character_iterator < $total_characters)
+            while( $character_iterator < $total_characters )
             {
 
                 //skip the index pertaining to "character_to_iterate"
