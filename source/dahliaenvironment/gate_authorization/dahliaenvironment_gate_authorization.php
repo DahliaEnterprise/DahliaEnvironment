27e2ad97314aae3249bfcdf64fcee770ec5ab8da
Shane Betz <dahliacommunication67@gmail.com>
2023-11-15 19:30:29 -0800

    *create a transaction that tracks the authorization progress(incomplete, almost finished) *update the mysql setup script regarding transactions of session authorization regarding create session

--- source/dahliaenvironment/gate_authorization/dahliaenvironment_gate_authorization.php
+++ source/dahliaenvironment/gate_authorization/dahliaenvironment_gate_authorization.php
@@ -8,21 +8,46 @@ class dahliaenvironment_gate_authorization
         $output["client_found"] = -1;
         $output["id"] = -1;
 
+        //hash password
+        $password_as_hash = hash("sha256", $password);
+        echo $phone_number."<br/>".$password_as_hash;
         $database_connection_handle = new PDO("mysql:host=".$database_host.";dbname=".$database_name.";", $database_username, $database_password);
-        $query_as_string = "SELECT `id` FROM ".$database_table_name." WHERE `phonenumber` = ? AND `password` = ? LIMIT 0,1";
-        $stmt = $database_connection_handle->prepare($query_as_string);
-        $stmt->bindParam(1, $phone_number, PDO::PARAM_INT);
-        $stmt->bindParam(2, $password, PDO::PARAM_STR);
+        $query_as_string_find_phonenumber_pair_match = "SELECT `id` FROM ".$database_table_name." WHERE `phonenumber` = ? AND `password` = ? LIMIT 0,1";
+        $stmt = $database_connection_handle->prepare($query_as_string_find_phonenumber_pair_match);
+        $stmt->bindParam(1, $phone_number, PDO::PARAM_STR);
+        $stmt->bindParam(2, $password_as_hash, PDO::PARAM_STR);
         $stmt->execute();
 
         $fetched_row = $stmt->fetch(PDO::FETCH_ASSOC);
         if($fetched_row != false)
         {
+            //define output information
             $output["client_found"] = 1;
             $output["id"] = (int)$fetched_row["id"];
+
+            //create a transaction to allow tracking the status and continue with authentication progress
+                //Create random string
+                $random_string_as_random_integer = random_int(1000, PHP_INT_MAX);
+                $random_string_as_random_integer_with_timestamp = time();
+                $random_string_as_random_integer_with_timestamp_as_hash = hash("sha256", $random_string_as_random_integer_with_timestamp);
+
+                //Prepare results as variables to clear input as passed-as-parameter
+                $sms_code = 123456;
+                $timestamp = time();
+
+            $query_as_string_create_transaction_for_tracking_status = "INSERT INTO `transactions_of_session_authorization_regarding_create_session` (`client_credentials_id`, `sha256_regarding_session_authorization_code_to_create_session`, `sha256_session_identification`, `timestamp`) VALUES(?, ?, ?, ?);";
+            $stmt2 = $database_connection_handle->prepare($query_as_string_create_transaction_for_tracking_status);
+            $stmt2->bindParam(1, $output["id"], PDO::PARAM_INT);
+            $stmt2->bindParam(2, $sms_code, PDO::PARAM_INT);
+            $stmt2->bindParam(3, $random_string_as_random_integer_with_timestamp_as_hash, PDO::PARAM_STR);
+
+            $stmt2->bindParam(4, $timestamp, PDO::PARAM_INT);
+            $stmt2->execute();
+
         }
 
         return $output;
     }
+
 }
 ?>
--- source/dahliaenvironment/mysql_initialization_scripts/transactions_of_session_authorization_regarding_create_session.sql
+++ source/dahliaenvironment/mysql_initialization_scripts/transactions_of_session_authorization_regarding_create_session.sql
@@ -1,1 +1,10 @@
-CREATE TABLE transactions_of_session_authorization_regarding_create_session (id BIGINT PRIMARY KEY AUTO_INCREMENT, client_credentials_id MEDIUMINT, sha256_regarding_session_authorization_code_to_create_session VARCHAR(64) UNIQUE, timestamp MEDIUMINT);
+CREATE TABLE `transactions_of_session_authorization_regarding_create_session` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `client_credentials_id` mediumint DEFAULT NULL,
+  `sha256_regarding_session_authorization_code_to_create_session` varchar(64) DEFAULT NULL,
+  `sha256_session_identification` varchar(64) DEFAULT NULL,
+  `timestamp` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `sha256_regarding_session_authorization_code_to_create_session` (`sha256_regarding_session_authorization_code_to_create_session`),
+  UNIQUE KEY `sha256_session_identification` (`sha256_session_identification`)
+)
