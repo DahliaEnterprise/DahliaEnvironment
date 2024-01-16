7f68c741bd9e0dd0ca47348a33076712a58661f6
Shane Betz <dahliacommunication67@gmail.com>
2024-01-16 07:17:15 -0800

    *auto increment modifier for the heritage_category_index table creation script.

--- source/mysql_table_creation_scripts/table_structure.sql
+++ source/mysql_table_creation_scripts/table_structure.sql
@@ -1,5 +1,5 @@
 CREATE TABLE heritage_category_index (
-    id INT PRIMARY KEY UNIQUE NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     internal_category_name VARCHAR(32),
     display_category_name VARCHAR(32),
     front_page_ordered_list_index_point INT
